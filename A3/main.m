b3adc5adf336bf620a4e1020b89ddda0aefb8866
Samuel Norling <norling.samuel@gmail.com>
2020-12-03 11:34:03 +0100

    update

--- A3/main.m
+++ A3/main.m
@@ -6,26 +6,28 @@ n_of_rs = 25;
 rs = linspace(3.00, 9.00, n_of_rs);
 optimal_sigmas = zeros(n_of_rs, 1);
 optimal_mus = zeros(n_of_rs, 1);
+n=8;
+lb = zeros(n, 1);
 
 for r_index=1:n_of_rs
     % Generate new data
     
-    n=8;
+    
     f_vector = zeros(n, 1);
     e_vector = ones(n, 1);
     Corr=zeros(n,n);
     for i=1:n
-    for j=1:n
-    Corr(i,j)=(-1)^abs(i-j)/(abs(i-j)+1);
-    end
+        for j=1:n
+            Corr(i,j)=(-1)^abs(i-j)/(abs(i-j)+1);
+        end
     end
     sigma=zeros(n,1);
     mu=zeros(n,1);
     sigma(1)=2;
     mu(1)=3;
     for i=1:n-1
-    sigma(i+1)=sigma(i)+2*rand;
-    mu(i+1)=mu(i)+1;
+        sigma(i+1)=sigma(i)+2*rand;
+        mu(i+1)=mu(i)+1;
     end
     D=diag(sigma);
     C2=D*Corr*D;
@@ -33,7 +35,7 @@ for r_index=1:n_of_rs
     % Find optimal values with quadprog
 	Aeq = [mu,e_vector]';
     beq = [rs(r_index); 1];
-    [x,fval,exitflag,output,lambda] = quadprog(C,f_vector,[],[],Aeq,beq);
+    [x,fval,exitflag,output,lambda] = quadprog(C,f_vector,[],[],Aeq,beq,lb);
     optimal_sigmas(r_index) = sqrt(fval);
     optimal_mus(r_index) = mu' * x;
 
@@ -42,3 +44,5 @@ end
 optimal_sigmas
 optimal_mus
 plot(optimal_sigmas, optimal_mus)
+xlabel('optimal mu')
+ylabel('optimal sigma')
